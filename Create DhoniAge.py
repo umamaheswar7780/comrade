6355aafad5d6d61662993eaa449b6d952c031f04
umamaheswar7780 <umamaheswar244@gmail.com>
2024-09-18 15:37:44 +0530

    Update Create DhoniAge.py

--- Create DhoniAge.py
+++ Create DhoniAge.py
@@ -1,5 +1,5 @@
 ...
-MS Dhoni aged X years, is a Cancerian, born with very strong Mars in his birth chart. Notably, Mars is the ruling planet for sports. Write a program to get the age of Dhoni as an integer and display the same.
+1)MS Dhoni aged X years, is a Cancerian, born with very strong Mars in his birth chart. Notably, Mars is the ruling planet for sports. Write a program to get the age of Dhoni as an integer and display the same.
 Input Format:
 Input is an integer that corresponds to the age of Dhoni.
 Output Format:
@@ -10,5 +10,9 @@ Sample Input and Output:
 Enter the age:
 35
 Age of Dhoni is 35
+Ans:
+a=int(input())
+print("Age of Dhoni is",a)
+
 
 ...
